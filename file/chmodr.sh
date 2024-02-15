99758266436966f378a322fbbb9ad3ebfa74c913
Gérald Maréchal <gerald1973@gmail.com>
2024-02-15 11:21:11 +0100

    Spell.

--- file/chmodr.sh
+++ file/chmodr.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
-cat << DESCRIPTION
-This scripts will modify the add or remove the permissions to the given path
-example:
-${0} /mydir/dir g+rwx,u+rwx g+rw,u+rw
-DESCRIPTION
 #Test the number of parameter
 if [[ ${#} != 3 ]]; then
+cat <<-DESCRIPTION
+This script modifies file permissions (read, write, execute) for a specified path and its contained files.
+Example:
+${0} /mydir/dir g+rwx,u+rwx g+rw,u+rw
+
+${0}: Represents the script's name.
+
+Directory part:
+===============
+
+g+rwx: Adds read, write, and execute permissions for the group.
+u+rwx: Adds read, write, and execute permissions for the owner.
+
+File part:
+==========
+
+g+rw: Adds read and write permissions for the group.
+u+rw: Adds read and write permissions for the owner.
+
+DESCRIPTION
   echo "Incorrect number of arguments"
   echo "valid example ${0} /mydir/dir g+rwx,u+rwx g+rw,u+rw"
   exit 1
 fi
-read -p "Do you wish change the permission(s) on the directory ${1} with the permission ${2} and all of its files and subdirectories (Y/N) ?" yn
+read -p "Do you want to change the permissions on the directory ${1} with the permissions ${2} and all of its files and subdirectories (Y/N)?" yn
 echo ${yn}
 echo ${1}
 echo ${2}
